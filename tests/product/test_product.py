4cf8b9d59e25ec3c3d104de8b5722b024a825721
Raphael <raphaellfpg@gmail.com>
2022-11-01 13:04:56 -0300

    test: Testa a funcao que cria o produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Almoço',
+        'Dom Supremo',
+        '01-11-2022',
+        '01-11-2022',
+        5495,
+        'Colocar na geladeira'
+        )
+    assert product.id == 1
+    assert product.data_de_validade == '01-11-2022'
+    assert product.data_de_fabricacao == '01-11-2022'
+    assert product.nome_da_empresa == 'Dom Supremo'
+    assert product.nome_do_produto == 'Almoço'
+    assert product.numero_de_serie == 5495
+    assert product.instrucoes_de_armazenamento == 'Colocar na geladeira'
