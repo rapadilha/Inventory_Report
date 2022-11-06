86d5c13d06461735f8d63b9e667af4c2f895c4c5
Raphael <raphaellfpg@gmail.com>
2022-11-06 18:10:11 -0300

    feat: adiciona leitor json e xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -7,7 +9,12 @@ class Inventory:
     def import_data(path, type):
         list = []
         with open(path, encoding='utf-8') as file:
-            data = csv.DictReader(file)
+            if path.endswith('csv'):
+                data = csv.DictReader(file)
+            elif path.endswith('json'):
+                data = json.load(file)
+            else:
+                data = xmltodict.parse(file.read())["dataset"]["record"]
             for row in data:
                 list.append(row)
         if type == 'simples':
